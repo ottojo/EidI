1433c03da6774ed24c84d4635ecc90703bad42e5
ottojo <jottosmail@gmail.com>
2017-11-11 23:57:24 +0100

    3.4 java program

---
+++ 3/src/GrumpyCat/GrumpyCat.java
@@ -0,0 +1,22 @@
+package GrumpyCat;
+
+public class GrumpyCat {
+
+    public static void main(String[] args) {
+        // Iteriere über alle Zahlen von 1 bis 100
+        for (int i = 1; i <= 100; i++) {
+            String output = "";
+            // Prüfen ob die Zahl durch 3 teilbar ist
+            if (i % 3 == 0)
+                output += "grumpy";
+            // Prüfen ob die Zahl durch 5 teilbar ist
+            if (i % 5 == 0)
+                output += "cat";
+            // Wenn die Zahl weder durch 3 oder 5 teilbar ist soll die Zahö ausgegeben werden
+            if (output.equals(""))
+                output += i;
+            //Ausgabe des Ergebnisses
+            System.out.println(output);
+        }
+    }
+}
