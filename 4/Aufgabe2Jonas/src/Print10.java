022707cb55f1ba2972f641c80aafd19399ca6df4
ottojo <jottosmail@gmail.com>
2017-11-17 14:39:38 +0100

    Print10 alternative

--- 4/Aufgabe2Jonas/src/Print10.java
+++ 4/Aufgabe2Jonas/src/Print10.java
@@ -1,13 +1,18 @@
 public class Print10 {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         int width = TextIO.getInt();
         int height = TextIO.getInt();
 
+        //Iteriere über Zeilen
         for (int h = 0; h < height; h++) {
+            //Iteriere über Spalten
             for (int w = 0; w < width; w++) {
+                // Ausgabe von U+2571 + Zufallszahl: 0 oder 1
+                // (U+2571 oder U+2572
                 System.out.print(Character.toChars(0x2571 + (int) (Math.random() * 2)));
             }
+            //Wenn über alle Spalten iteriert: neue Zeile
             System.out.println();
         }
     }
