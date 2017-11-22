e2e270d02957c608642838071311c99f9fe181c8
Marco5de <33183479+Marco5de@users.noreply.github.com>
2017-11-22 21:00:42 +0100

    Update NTuermeProblem.java

--- 5/Aufgabe3/src/NTuermeProblem.java
+++ 5/Aufgabe3/src/NTuermeProblem.java
@@ -1,10 +1,12 @@
 
 public class NTuermeProblem {
 	public static void main(String[]args) {
+		//Groesse des Schachbrettes wird festgelegt
 		final int N = 8;
 		boolean[][]brett = new boolean[N][N];
 		boolean korrekt = true;
 		
+		//Stellt zufällig Tuerme aufs Feld. Fuer Testzwecke
 		for(int z=0;z<N;z++) {
 			int i = (int)(Math.random()*N);
 			int j = (int)(Math.random()*N);
@@ -12,22 +14,26 @@ public class NTuermeProblem {
 			System.out.println("Turm: " + i + " " + j);
 		}
 		
-		
+		//Verschachtelte Vorschleife geht alle Felder auf Brett durch
 		for(int i=0;i<N;i++) {
 			int colcounter = 0;
 			int rowcounter = 0;
 			if(!korrekt)
 				break;
 			for(int j=0;j<N;j++) {
+				//wenn auf Feld ein Turm steht wird Counter erhöht 
 				if(brett[j][i])
 					colcounter++;
+				//Wenn in einer Reihe/Zeile mehr als ein Turm steht wird korrekt auf false gesetzt
 				if (colcounter>1) {
 					korrekt = false;
 					System.out.println("Mehr als ein Turm in der gleichen Spalte");
 					break;
 				}
+				//wenn auf Feld ein Turm steht wird Counter erhöh
 				if(brett[i][j])
 					rowcounter++;
+				//Wenn in einer Reihe/Zeile mehr als ein Turm steht wird korrekt auf false gesetzt
 				if(rowcounter>1) {
 					korrekt=false;
 					System.out.println("Mehr als ein Turm in der gleichen Zeile ");
