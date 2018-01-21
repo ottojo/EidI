554ce11c004e69ddb9587701d17c67b484cb441b
ottojo <jottosmail@gmail.com>
2018-01-21 18:30:38 +0100

    optimized/renamed stuff

--- 11/Aufgabe1_2_Marco/src/Testclass.java
+++ 11/Aufgabe1_2_Marco/src/Testclass.java
@@ -1,39 +1,36 @@
 //package blatt11;
 
 public class Testclass {
-	public static void main(String[]args) {
-		//Erstellt zwei Figuren und zwei Waffen mit gegebenen Eigenschaften
-		Figur Marco = new Figur("Marco","Mensch",85,12,10);
-		Figur Ork = new Figur("Ork","Ork",50,14,6);
-		
-		Waffe schwert = new Waffe("Maechtiges Schwert",15);
-		Waffe axt = new Waffe("Groﬂe Axt",7);
-		
-		//System.out.println(Ork.getArmor());
-		Marco.equipWeapon(schwert);
-		Ork.equipWeapon(axt);
-		
-		//Figuren kaempfen solange gegeneinander bis eine Tod ist oder die Waffe einer Figur kaputt ist
-		while(Marco.checkPlayerAlive() && Ork.checkPlayerAlive() && Marco.getWeapon().checkWeapon() && Ork.getWeapon().checkWeapon()) {
-			Marco.attack(Ork);
-			System.out.println("Marcos HP: " + Marco.getHP()+ "			 Marcos Waffenabnutzung: " + Marco.getWeapon().getPCT());
-			System.out.println("Orks HP: " + Ork.getHP()+ " 		Orks Waffenabnutzung: " + Ork.getWeapon().getPCT());
-			System.out.println();
-			if(Marco.checkPlayerAlive() && Ork.checkPlayerAlive() && Marco.getWeapon().checkWeapon() && Ork.getWeapon().checkWeapon()) {
-				Ork.attack(Marco);
-				System.out.println("Marcos HP: " + Marco.getHP()+ " 		Marcos Waffenabnutzung: " + Marco.getWeapon().getPCT());
-				System.out.println("Orks HP: " + Ork.getHP() +  " 		Orks Waffenabnutzung: " + Ork.getWeapon().getPCT());
-				System.out.println();
-			}
-			
-		}
-		//Ausgabe welche Figur gestorben ist
-		if(!Marco.checkPlayerAlive())
-			System.out.println("Marco ist tod");
-		if(!Ork.checkPlayerAlive())
-			System.out.println("Ork ist tod");
-		
-		
-		
-	}
+    public static void main(String[] args) {
+        //Erstellt zwei Figuren und zwei Waffen mit gegebenen Eigenschaften
+        Figur Marco = new Figur("Marco", "Mensch", 85, 12, 10);
+        Figur Ork = new Figur("Ork", "Ork", 50, 14, 6);
+
+        Waffe schwert = new Waffe("Maechtiges Schwert", 15);
+        Waffe axt = new Waffe("Gro√üe Axt", 7);
+
+        //System.out.println(Ork.getArmor());
+        Marco.waffeAusruesten(schwert);
+        Ork.waffeAusruesten(axt);
+
+        //Figuren kaempfen solange gegeneinander bis eine Tod ist oder die Waffe einer Figur kaputt ist
+        while (Marco.isAlive() && Ork.isAlive() && !Marco.getWaffe().isBroken() && !Ork.getWaffe().isBroken()) {
+            Marco.angreifen(Ork);
+            System.out.println("Marcos HP: " + Marco.getLebensPunkte() + "			 Marcos Waffenabnutzung: " + Marco.getWaffe().getPCT());
+            System.out.println("Orks HP: " + Ork.getLebensPunkte() + " 		Orks Waffenabnutzung: " + Ork.getWaffe().getPCT());
+            System.out.println();
+            if (Marco.isAlive() && Ork.isAlive() && !Marco.getWaffe().isBroken() && !Ork.getWaffe().isBroken()) {
+                Ork.angreifen(Marco);
+                System.out.println("Marcos HP: " + Marco.getLebensPunkte() + " 		Marcos Waffenabnutzung: " + Marco.getWaffe().getPCT());
+                System.out.println("Orks HP: " + Ork.getLebensPunkte() + " 		Orks Waffenabnutzung: " + Ork.getWaffe().getPCT());
+                System.out.println();
+            }
+
+        }
+        //Ausgabe welche Figur gestorben ist
+        if (!Marco.isAlive())
+            System.out.println("Marco ist tod");
+        if (!Ork.isAlive())
+            System.out.println("Ork ist tod");
+    }
 }
--- 11/Aufgabe1_2_Marco/src/Waffe.java
+++ 11/Aufgabe1_2_Marco/src/Waffe.java
@@ -1,60 +1,51 @@
-//package blatt11;
-import java.util.Random;
-
 public class Waffe {
-	Random rand = new Random();
-	//Attribute einer Waffe
-	private String name;
-	private int dmg;
-	private int pct;
-	
-	
-	//Konstruktor zum erstellen einer Waffe
-	Waffe(String name, int dmg){
-		this.name = name;
-		this.dmg = dmg;
-		this.pct = 100;
-	}
-	
-	//Im Folgenden werden die getter/setter Methoden f�r eine Waffe erstellt
-	protected String getName() {
-		return this.name;
-	}
-	protected void setName(String name) {
-		this.name = name;
-	}
-	protected int getDmg() {
-		return this.dmg;
-	}
-	protected void setDmg(int dmg) {
-		this.dmg = dmg;
-	}
-	protected int getPCT() {
-		return this.pct;
-	}
-	protected void setPCT(int n) {
-		this.pct-=n;
-	}
-	
-	/**
-	 * Berechnet den Schaden den die Figur mit der entsprechende Waffe verursachen kann
-	 * @param sp Staerkepunkte des Spielers 
-	 * @return gibt Schadenswert zurueck
-	 */
-	protected int compDMG(int sp) {
-		//System.out.println(sp);
-		 int dmg = rand.nextInt(this.getDmg())+1;
-		 dmg = dmg + (sp/2);
-		return dmg;
-	}
-	/**
-	 * Pr�ft ob Waffe noch nicht kaputt
-	 * @return -Wert ob Waffe noch funktional
-	 */
-	protected boolean checkWeapon() {
-		if(this.pct>0)
-			return true;
-		else
-			return false;
-	}
+
+    //Attribute einer Waffe
+    private String name;
+    private int schadensPunkte;
+    private int statusPercent;
+
+
+    //Konstruktor zum erstellen einer Waffe
+    public Waffe(String name, int schadensPunkte) {
+        this.name = name;
+        this.schadensPunkte = schadensPunkte;
+        this.statusPercent = 100;
+    }
+
+    //Im Folgenden werden die getter/setter Methoden für eine Waffe erstellt
+    public String getName() {
+        return this.name;
+    }
+
+    public int getSchadensPunkte() {
+        return this.schadensPunkte;
+    }
+
+    public int getPCT() {
+        return this.statusPercent;
+    }
+
+
+    /**
+     * Waffe benutzen und Schaden berechnen
+     *
+     * @param staerkePunkte Stärke Punkte des Trägers
+     * @return Schaden, der durch die Waffe verursacht wird
+     */
+    public int useWeapon(int staerkePunkte) {
+        int damage = (int) (staerkePunkte * 0.5) + (int) (Math.random() * this.schadensPunkte + 1);
+        this.statusPercent -= 5;
+
+        return damage;
+    }
+
+    /**
+     * Prüft ob Waffe kaputt ist
+     *
+     * @return true, wenn die Waffe kaputt ist
+     */
+    protected boolean isBroken() {
+        return this.statusPercent <= 0;
+    }
 }
